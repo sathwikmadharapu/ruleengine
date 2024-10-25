0ab58a9ad312bbf4200c2cbdc308351154550518
sathwikmadharapu <sathwikmadharapu@gmail.com>
2024-10-26 00:22:38 +0530

    Create ast.py

---
+++ ast.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self, node_type, left=None, right=None, value=None):
+        self.type = node_type
+        self.left = left
+        self.right = right
+        self.value = value
